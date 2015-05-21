18cb7f040158cf4f533860f688cfa2d94de5e49d
Emilio Del Tessandoro <emilio.deltessa@gmail.com>
2015-05-21 15:02:49 +0200

    solved ipt of may

--- 2015.05/game.cpp
+++ 2015.05/game.cpp
@@ -2,19 +2,13 @@
 #include <algorithm>
 using namespace std;
 
-union conf {
-    uint16_t c[4];
-    uint64_t aggregate;
+struct state {
+    uint16_t c[3];
 
-    conf(uint64_t n) : aggregate(n) {
-        sort(c, c + 3);
-    }
-
-    conf(uint16_t x, uint16_t y, uint16_t z) {
+    state( uint16_t x, uint16_t y, uint16_t z ) {
         c[0] = x;
         c[1] = y;
         c[2] = z;
-        c[3] = 0;
         sort(c, c + 3);
     }
 
@@ -22,44 +16,51 @@ union conf {
         return c[0] != 0 && ( c[0] != c[1] && c[1] != c[2]);
     }
 
-    friend ostream& operator << ( ostream& o, const conf& c ) {
+    uint16_t operator [] ( const int i ) const { 
+        return c[i]; 
+    }
+
+    bool operator == ( const state& another ) const {
+        return equal(c, c + 3, another.c);
+    }
+
+    friend ostream& operator << ( ostream& o, const state& c ) {
         return o << c.c[0] << " " << c.c[1] << " " << c.c[2];
     }
 };
 
-int simulate ( conf c, int minutes, int limit ) {
+int simulate ( state c, int minutes, int limit ) {
     if ( ! c.valid() ) return minutes;
     if ( minutes == limit ) return limit;
 
-    int x = simulate( conf(c.c[0], c.c[1] * 2, c.c[2] - c.c[1]), minutes + 5, limit );
-    int y = simulate( conf(c.c[0] * 2, c.c[1], c.c[2] - c.c[0]), minutes + 5, limit );
-    int z = simulate( conf(c.c[0] * 2, c.c[1] - c.c[0], c.c[2]), minutes + 5, limit );
+    int x = simulate( state(c[0], c[1] * 2, c[2] - c[1]), minutes + 5, limit );
+    int y = simulate( state(c[0] * 2, c[1], c[2] - c[0]), minutes + 5, limit );
+    int z = simulate( state(c[0] * 2, c[1] - c[0], c[2]), minutes + 5, limit );
 
     return min(x, min(y, z));
 }
 
 int main ( ) {
     vector<int> results;
-    vector<uint64_t> all;
+    vector<state> all;
+
     for ( int x = 175; x <= 255; ++x )
         for ( int y = x + 1; y <= 255; ++y )
             for ( int z = y + 1; z <= 255; ++z )
-                all.push_back(conf(x,y,z).aggregate);
+                all.push_back(state(x, y, z));
 
-    for ( int limit = 25; limit < 60; limit += 5) {
+    for ( int limit = 10; limit < 60; limit += 5) {
         results.clear();
 
         for ( auto i : all )
-            results.push_back( simulate(conf(i), 0, limit) );
+            results.push_back( simulate(state(i), 0, limit) );
 
         for ( uint i = 0; i < results.size(); ++i )
             if ( results[i] < limit )
-                all[i] = 0;
+                all[i] = state(0, 0, 0);
 
-        all.erase(remove(all.begin(), all.end(), 0), all.end());
+        all.erase(remove(all.begin(), all.end(), state(0, 0, 0)), all.end());
     }
 
-    for ( auto i : all ) cout << conf(i) << endl;
-
-    return 0;
+    for ( auto i : all ) cout << state(i) << endl;
 }
