666a61f01b3b972fa542aa4070728e8174763bfd
Emilio Del Tessandoro <emilio@spotify.com>
2016-03-12 13:51:16 +0100

    solved

--- 2016/march/squareversed.cpp
+++ 2016/march/squareversed.cpp
@@ -2,43 +2,53 @@
 #include <array>
 #include <iostream>
 #include <vector>
+#include <gmpxx.h>
+#include <limits>       // std::numeric_limits
 using namespace std;
 
-#define DIGITS 16
-
-
-long long reverse(long long n) {
-  long long result = 0;
+template<class T>
+T reverse(T n, int digits ) {
+  T result = 0;
   do {
     result *= 10;
     result += n % 10;
     n /= 10;
-  } while( n );
+    digits--;
+  } while( digits );
 
   return result;
 }
 
 
-bool check( long long x, long long y, long long modulus) {
-    return y % modulus == reverse(x);
-}
-
-void naive( long long start) {
-    long long modulus = 10;
-
-    while(start >= 0) {
-        if (start > modulus)
-            modulus *= 10;
-
-        if ( check(start, start * start, modulus) )
-            cout << start << " " << start * start << endl;
-
-        start += 1;
-    }
+bool check( mpz_class x, mpz_class y, int digits) {
+    mpz_class h;
+    mpz_ui_pow_ui (h.get_mpz_t(), 10, digits);
+    return y % h == reverse(x, digits);
 }
 
 int main ( ) {
-  cout << reverse(123918231298301230L) << endl;
+    cout << numeric_limits<uint128_t>::max() << endl;
+    cout << numeric_limits<unsigned int>::max() << endl;
+    int digits = 11;
+    mpz_class h, l;
+    mpz_ui_pow_ui (h.get_mpz_t(), 10, digits);
+    mpz_ui_pow_ui (l.get_mpz_t(), 10, digits - 1);
 
-  naive(0);
+    for (mpz_class i = 0; i < h; ++i ) {
+        mpz_class j = reverse((i * i) % h, digits);
+
+        mpz_class c = i + j * h;
+        // cout << i << " " << j << " " << c << endl;
+        if ( c > 1 && check(c, c * c, 2 * digits) )
+           cout << i << " " << c << endl;
+
+        c = i + (j - j % 10) * l;
+        if ( c > 1 && check(c, c * c, 2 * digits - 1) )
+           cout << i << " " << c << endl;
+    }
 }
+
+// 15: 182921919071841
+// 15: 655785969669834
+// 18: 650700037578750084
+// 21: 125631041500927357539
--- 2016/march/squareversed.py
+++ 2016/march/squareversed.py
@@ -1,27 +1,29 @@
 import math
 
-def naive(start):
-    modulus = 10
-    while start >= 0:
-        if start > modulus:
-            modulus *= 10
-        if check(start, start**2, modulus):
-            print(start, start**2)
-        start += 1
+def check(x, y, digits):
+    return y % (10 ** digits) == reverse(x, digits)
 
-def check(x, y, modulus):
-    return y % modulus == reverse(x)
-    #return str(y).endswith(str(x)[::-1])
-
-def reverse(n):
+def reverse(n, digits):
     result = 0
-    while n > 0:
+    while digits:
         result *= 10
         result += n % 10
         n = math.floor(n/10)
+        digits -= 1
 
     return result
 
 if __name__ == "__main__":
-    print(reverse(1231239128))
-    naive(0)
+    digits = 7
+
+    for i in range(1, 10 ** digits):
+        j = i * i % (10 ** digits)
+        j = reverse(j, digits)
+
+        c = i + j * (10 ** digits)
+        if ( check(c, c * c, 2 * digits)):
+            print(c)
+
+        c = i + (j - j % 10) * (10 ** (digits - 1))
+        if ( check(c, c * c, 2 * digits - 1)):
+            print(c)
