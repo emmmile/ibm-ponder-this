55f05ba2d914da4064b217a86f7c4b0ef18f5c5b
Emilio Del Tessandoro <emilio.deltessa@gmail.com>
2016-09-26 21:21:51 +0200

    working on september solution

--- 2016/september/fourpeople.py
+++ 2016/september/fourpeople.py
@@ -1,5 +1,4 @@
-#!/usr/bin/python
-# 4 ['cdba', 'abcd', 'bcda', 'cbad', 'adcb', 'bcad', 'badc', 'cdab', 'acbd', 'bdac', 'dcba', 'cadb', 'dbca', 'adbc', 'dacb', 'abdc', 'cabd', 'dcab', 'dabc', 'bacd', 'dbac', 'bdca', 'acdb', 'cbda']
+#!/usr/bin/python3
 
 import itertools
 import math
@@ -15,31 +14,63 @@ def pairwise(iterable):
 # can be even a pair of seats, like ["abcd", "dcba"]
 def violations(seats):
     count = 0
-    for a, b in pairwise(seats):
+    for a, b in itertools.product(seats[:-1], [seats[-1]]):
         for ai, bi in zip(a, b):
             if ai is bi:
                 count += 1
     return count
+    # return count
+    # count = 0
+    # for a, b in pairwise(seats):
+    #     for ai, bi in zip(a, b):
+    #         if ai is bi:
+    #             count += 1
+    # return count
 
-def possible_seats():
+def seats():
     return ["".join(s) for s in list(itertools.permutations("abcd"))]
 
-if __name__ == "__main__":
-    print(sorted(["cdba","abdc","bdca","cbad","adbc","cbda","bdac","acdb","bacd","cdab","bcda","dacb","acbd","dbac","cadb","abcd","dcab","badc","dcba","adcb","dabc","bcad","dbca","cabd"]))
-    seats = possible_seats()
-    print(seats)
-    best = 24 * 4
-    while True:
-        random.shuffle(seats)
-        candidate = [seats[0]]
-        while len(candidate) < 24:
-            minimum = 4
-            another = None
-            for s in seats:
-                if s not in candidate and violations([candidate[-1], s]) < minimum:
-                    another = s
-            candidate.append(another)
+def pairs():
+    return [s for s in itertools.permutations(seats(), 2)]
 
-        if violations(candidate) <= best:
-            best = violations(candidate)
-            print(violations(candidate), candidate)
+def triples():
+    return [s for s in itertools.permutations(seats(), 3)]
+
+if __name__ == "__main__":
+    # given a pair like ["abcd", "dcba"] or a triple,, how many violations
+    # add the last element?
+    cache = {}
+
+    for i in pairs() + triples():
+        cache[i] = violations(i)
+
+    candidates = {(s,): 0 for s in seats()}
+    while True:
+        new_candidates = {}
+        for s in candidates.keys():
+            for another in seats():
+                if another not in s:
+                    newone = s[-2:] + (another,)
+                    new_candidates[newone] = cache[newone] + candidates[s]
+        candidates = new_candidates
+        print(len(new_candidates))
+
+
+
+    # seats = possible_seats()
+    # print(seats)
+    # best = 24 * 4
+    # while True:
+    #     random.shuffle(seats)
+    #     candidate = [seats[0]]
+    #     while len(candidate) < 24:
+    #         minimum = 4
+    #         another = None
+    #         for s in seats:
+    #             if s not in candidate and violations([candidate[-1], s]) < minimum:
+    #                 another = s
+    #         candidate.append(another)
+    #
+    #     if violations(candidate) <= best:
+    #         best = violations(candidate)
+    #         print(violations(candidate), candidate)
