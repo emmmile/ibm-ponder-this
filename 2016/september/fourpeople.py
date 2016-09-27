e192dfa84ac259950ad6c1d92392985e0d7efb8a
Emilio Del Tessandoro <emilio@spotify.com>
2016-09-27 12:22:38 +0200

    some comments

--- 2016/september/fourpeople.py
+++ 2016/september/fourpeople.py
@@ -11,37 +11,36 @@ def violations(seats):
                 count += 1
     return count
 
-def seats():
+def singles():
     return [''.join(s) for s in itertools.permutations('ABCD')]
 
 def pairs():
-    return [s for s in itertools.permutations(seats(), 2)]
+    return [s for s in itertools.permutations(singles(), 2)]
 
 def triples():
-    return [s for s in itertools.permutations(seats(), 3)]
+    return [s for s in itertools.permutations(singles(), 3)]
 
+# generates the solutions one by one
 def loop(candidate, suffixes, cost):
     if len(candidate) == 24:
         yield cost, candidate
     else:
-        for s in seats():
+        for s in singles():
             if s not in candidate:
                 newone = candidate[-2:] + (s,)
                 newcost = suffixes[newone] + cost
-                if newcost < 1:
+                # go on with the ones that has no more than this violations
+                if newcost < 2:
                     for i in loop(candidate + (s,), suffixes, newcost):
                         yield i
 
 if __name__ == "__main__":
-    # given a pair like ['abcd', 'dcba'] or a triple, how many violations
-    # the last element contributes to?
     suffixes = {}
     for i in pairs() + triples():
         suffixes[i] = violations(i)
 
-    # chain the generators from every starting seat
-    generator = itertools.chain(*[loop((s,), suffixes, 0) for s in seats()])
+    # chain the generators from any starting point
+    solutions = itertools.chain(*[loop((s,), suffixes, 0) for s in singles()])
 
-    # print the solutions
-    for cost, seat in generator:
-        print(cost, seat)
+    for violations, seats in solutions:
+        print(violations, seats)
