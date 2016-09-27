7fa82d5c245aa12aecf8291653d2d1706f48dd30
Emilio Del Tessandoro <emilio@spotify.com>
2016-09-27 14:32:03 +0200

    bit easier to read

--- 2016/september/fourpeople.py
+++ 2016/september/fourpeople.py
@@ -2,7 +2,6 @@
 
 import itertools
 
-# how many violations does the last seat add?
 def violations(seats):
     count = 0
     for a, b in itertools.product(seats[:-1], [seats[-1]]):
@@ -20,18 +19,17 @@ def pairs():
 def triples():
     return [s for s in itertools.permutations(singles(), 3)]
 
-# generates the solutions one by one
 def loop(candidate, suffixes, cost):
     if len(candidate) == 24:
         yield cost, candidate
     else:
         for s in singles():
             if s not in candidate:
-                newone = candidate[-2:] + (s,)
-                newcost = suffixes[newone] + cost
-                # go on with the ones that has no more than this violations
+                suffix = candidate[-2:] + (s,)
+                newcost = suffixes[suffix] + cost
                 if newcost < 2:
-                    for i in loop(candidate + (s,), suffixes, newcost):
+                    newone = candidate + (s,)
+                    for i in loop(newone, suffixes, newcost):
                         yield i
 
 if __name__ == "__main__":
@@ -39,8 +37,6 @@ if __name__ == "__main__":
     for i in pairs() + triples():
         suffixes[i] = violations(i)
 
-    # chain the generators from any starting point
-    solutions = itertools.chain(*[loop((s,), suffixes, 0) for s in singles()])
-
-    for violations, seats in solutions:
-        print(violations, seats)
+    generators = [loop((s,), suffixes, 0) for s in singles()]
+    solutions = itertools.chain(*generators)
+    print(next(solutions))
